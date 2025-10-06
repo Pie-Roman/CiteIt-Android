2f2ddb97b398c5232914497a897aab76039b3fef
Pie-Roman <py.romann@yandex.ru>
2025-10-06 20:18:45 +0300

    синхронизировал анимацию смахивания цитаты с состоянием списка

--- feature/main-feature/src/main/kotlin/ru/pyroman/citeit/feature/main/view/MainView.kt
+++ feature/main-feature/src/main/kotlin/ru/pyroman/citeit/feature/main/view/MainView.kt
@@ -1,11 +1,31 @@
 package ru.pyroman.citeit.feature.main.view
 
+import androidx.compose.animation.core.Animatable
+import androidx.compose.animation.core.AnimationVector1D
+import androidx.compose.animation.core.LinearOutSlowInEasing
+import androidx.compose.animation.core.tween
+import androidx.compose.foundation.gestures.detectDragGestures
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.derivedStateOf
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.key
+import androidx.compose.runtime.mutableStateMapOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.graphicsLayer
+import androidx.compose.ui.input.pointer.pointerInput
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.zIndex
 import androidx.hilt.lifecycle.viewmodel.compose.hiltViewModel
 import androidx.navigation.NavController
+import kotlinx.coroutines.launch
 import ru.pyroman.citeit.feature.main.viewmodel.MainViewModel
 
 @Composable
@@ -20,10 +40,79 @@ fun MainView(
         viewModel.fetchQuoteList()
     }
 
-    QuotesStackView(
-        quotes = quoteList,
-        onQuoteDragEnd = {
-            viewModel.removeLastQuote()
-        },
-    )
+    val count = quoteList.size
+    val numberOfVisible = 3
+
+    val offsets = remember { mutableStateMapOf<String, Animatable<Float, AnimationVector1D>>() }
+    val scope = rememberCoroutineScope()
+
+    if (count > 0) {
+        Box(
+            modifier = Modifier.fillMaxSize(),
+            contentAlignment = Alignment.Center
+        ) {
+            quoteList.forEachIndexed { index, quote ->
+                val reverseIndex = quoteList.lastIndex - index
+
+                val offsetX = offsets.getOrPut(quote.id) { Animatable(index * 40f) }
+                val rotation = derivedStateOf { offsetX.value / 10f }
+
+                if (index < numberOfVisible) {
+                    key(quote.id) {
+                        QuoteView(
+                            quote = quote,
+                            modifier = Modifier
+                                .graphicsLayer {
+                                    translationX = offsetX.value
+                                    rotationZ = rotation.value
+                                }
+                                .pointerInput(Unit) {
+                                    if (index == 0) {
+                                        detectDragGestures(
+                                            onDrag = { change, dragAmount ->
+                                                change.consume()
+                                                if (dragAmount.x < 0) {
+                                                    scope.launch { offsetX.snapTo(offsetX.value + dragAmount.x) }
+                                                }
+                                            },
+                                            onDragEnd = {
+                                                if (offsetX.value < -size.width / 3) {
+                                                    scope.launch {
+                                                        offsetX.animateTo(
+                                                            targetValue = -1000f,
+                                                            animationSpec = tween(
+                                                                durationMillis = 400,
+                                                                easing = LinearOutSlowInEasing
+                                                            )
+                                                        )
+                                                        viewModel.removeLastQuote()
+                                                    }
+                                                } else {
+                                                    scope.launch {
+                                                        offsetX.animateTo(
+                                                            targetValue = 0f,
+                                                            animationSpec = tween(
+                                                                durationMillis = 400,
+                                                                easing = LinearOutSlowInEasing
+                                                            )
+                                                        )
+                                                    }
+                                                }
+                                            }
+                                        )
+                                    }
+                                }
+                                .padding(all = (index * 20).dp)
+                                .zIndex(reverseIndex.toFloat()),
+                            isTextShown = index < 2,
+                        )
+                    }
+
+                    scope.launch {
+                        offsetX.animateTo(index * 35f, tween(400))
+                    }
+                }
+            }
+        }
+    }
 }
--- feature/main-feature/src/main/kotlin/ru/pyroman/citeit/feature/main/view/QuoteView.kt
+++ feature/main-feature/src/main/kotlin/ru/pyroman/citeit/feature/main/view/QuoteView.kt
@@ -1,8 +1,6 @@
 package ru.pyroman.citeit.feature.main.view
 
-import androidx.compose.animation.core.animateFloatAsState
 import androidx.compose.foundation.background
-import androidx.compose.foundation.gestures.detectDragGestures
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -10,85 +8,38 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableFloatStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.graphicsLayer
-import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.platform.LocalWindowInfo
 import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.unit.IntOffset
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import androidx.compose.ui.zIndex
 import ru.pyroman.citeit.domain.quote.model.Quote
-import kotlin.math.abs
-import kotlin.math.roundToInt
 
 @Composable
 fun QuoteView(
     quote: Quote,
-    index: Int,
-    reverseIndex: Int,
-    textPadding: Float,
+    modifier: Modifier,
     isTextShown: Boolean,
-    onDragEnd: () -> Unit,
 ) {
-    var offsetX by remember { mutableFloatStateOf((index * 40).toFloat()) }
-    var rotation by remember { mutableFloatStateOf((index * 8).toFloat()) }
-
-    val animatedOffsetX by animateFloatAsState(targetValue = offsetX)
-    val animatedRotation by animateFloatAsState(targetValue = rotation)
-
     val density = LocalDensity.current
     val screenWidth = LocalWindowInfo.current.containerSize.width
     val screenHeight = LocalWindowInfo.current.containerSize.height
 
-    val width = with(density) { (screenWidth * 0.75).toFloat().toDp() }
-    val height = with(density) { (screenHeight * 0.5).toFloat().toDp() }
+    val width = with(density) { (screenWidth * 0.75f).toFloat().toDp() }
+    val height = with(density) { (screenHeight * 0.5f).toFloat().toDp() }
 
     Box(
-        modifier = Modifier
-            .offset { IntOffset(animatedOffsetX.roundToInt(), 0) }
-            .pointerInput(Unit) {
-                detectDragGestures(
-                    onDrag = { change, dragAmount ->
-                        change.consume()
-                        if (dragAmount.x < 0) {
-                            offsetX += dragAmount.x
-                            rotation = offsetX / 10f
-                        }
-                    },
-                    onDragEnd = {
-                        if (offsetX < 0 && abs(offsetX) > size.width / 3) {
-                            offsetX = -1000f
-                            onDragEnd()
-                        } else {
-                            offsetX = 0f
-                            rotation = 0f
-                        }
-                    }
-                )
-            }
-            .padding(all = (index * 20).dp)
-            .graphicsLayer {
-                translationX = animatedOffsetX
-                rotationZ = animatedRotation
-            }
+        modifier = modifier
             .width(width)
             .height(height)
-            .zIndex(reverseIndex.toFloat())
             .padding(20.dp)
             .clip(RoundedCornerShape(20.dp))
             .background(Color(quote.color.red, quote.color.green, quote.color.blue)),
@@ -120,7 +71,7 @@ fun QuoteView(
                     letterSpacing = 1.sp,
                     lineHeight = 30.sp,
                     modifier = Modifier
-                        .padding(textPadding.dp)
+                        .padding(20.dp)
                         .fillMaxWidth(),
                     textAlign = TextAlign.Start,
                 )
